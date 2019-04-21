d141995f33cce889088e6bca4bd478af2a495fc1
drejrnal <48739574+drejrnal@users.noreply.github.com>
2019-04-21 18:47:17 +0800

    Update WordLadder.java

--- Artsweek4/WordLadder.java
+++ Artsweek4/WordLadder.java
@@ -2,21 +2,16 @@ package GraphTheory;
 
 import java.util.*;
 
-/**
- * @Author:毛翔宇
- * @Date 2019-03-22 10:43
- */
+
 public class WordLadder {
 
     private HashMap<String, LinkedList<String>> adjacents;
     private HashMap<String,Integer> distanceMap;
     private HashSet<String> dictionary;
     private List<List<String>> solution;
-    private HashMap<String,String> edgeTo;
     public WordLadder(){
         adjacents = new HashMap<>();
         distanceMap  = new HashMap<>();
-        edgeTo = new HashMap<>();
         solution = new LinkedList<>();
     }
     public void bfs(String beginWord, String endWord, List<String> wordList){
@@ -39,7 +34,7 @@ public class WordLadder {
                          */
                     }
                     else {
-                        edgeTo.put(adj,source);
+                    
                         queue.offer(adj);
                     }
                 }
@@ -61,25 +56,10 @@ public class WordLadder {
         }
     }
 
-    public Iterable<String> pathTo(String beginWord,String endWord){
-        LinkedList<String> res = new LinkedList<>();
-        if (!distanceMap.containsKey(endWord))
-            return res;
-        Stack<String> path = new Stack<>();
-        for (String route = endWord;;route = edgeTo.get(route)){
-            if (route.equals(beginWord))
-                break;
-            path.push(route);
-        }
-        path.push(beginWord);
-        while (!path.isEmpty())
-            res.add(path.pop());
-        return res;
-    }
-
+    
     public LinkedList<String> findOptionsFromDict(String steorotype,Set<String> candidates){
         LinkedList<String> elected = new LinkedList<>();
-        /*for (int i = 0;i<steorotype.length();i++){
+        for (int i = 0;i<steorotype.length();i++){
             char[] steoro = steorotype.toCharArray();
             for (char j = 'a';j<='z';j++){
                     steoro[i] = j;
@@ -87,30 +67,8 @@ public class WordLadder {
                     if (!steorotype.equals(transformed) && candidates.contains(transformed))
                         elected.offer(transformed);
             }
-        }*/
-        for(String candidate:candidates){
-            int diff = 0;
-            for (int i = 0;i<candidate.length();i++)
-                if (candidate.charAt(i) != steorotype.charAt(i))
-                    diff+=1;
-            if (diff == 1)
-                elected.add(candidate);
         }
         return elected;
     }
-    public static void main(String[] args){
-        WordLadder wordLadder = new WordLadder();
-        String begin = "hit";
-        String end = "cog";
-        String[] dictionary = {"hot","dot","dog","lot","log","cog"};
-        //System.out.println(Arrays.asList(dictionary));
-        List<List<String>> solution = new LinkedList<>();
-        wordLadder.bfs(begin,end,Arrays.asList(dictionary));
-        LinkedList<String> path = new LinkedList<>();
-        path.add(begin);
-        System.out.println(wordLadder.distanceMap.get(end));
-        //wordLadder.dfs(begin,end,path);
-        //System.out.println(wordLadder.solution);
-
-    }
+    
 }
